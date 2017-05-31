5438122990ddd80797184a00dd3a8f47a3c0e55c
gabrielaivascu <gabriela.ivascu95@gmail.com>
2017-05-31 13:00:15 +0300

    Update and rename acasa.html to acasa.php

--- acasa.php
+++ acasa.php
@@ -16,36 +16,35 @@
 						<ul id="meniu-ul">
 
 							<li id="meniu-li"><img src="logo.png" alt="logo"></li>
-							<li id="meniu-li"><a href="acasa.html"> Acasa</a></li>
+							<li id="meniu-li"><a href="acasa.php"> Acasa</a></li>
 							<li id="meniu-li"><a href="contulmeu.html">Contul meu</a></li>
 							<li id="meniu-li"><a href="formular.html">Formular</a></li>
 							<li id="meniu-li"><a href="filtrare.html">Statistici</a></li>
 							<li id="meniu-li"><a href="deconectare.html">Deconectare</a></li>
 						</ul>
-							  
+
 				  </div>
-				  
-				  
+
+				<h1>Bine ai venit, <?php session_start(); echo $_SESSION['nume']; echo " "; echo $_SESSION['prenume']; ?>!</h1>
+
+
+				<h2>Pentru ca aplicatia sa fie pe placul dumneavoastra, va rugam sa completati formularul urmator <a href="review.html">Feedback</a>.</h2>
 			<br><br>
-			
-			
-			<p>ANUNTURI IN CEAPA MATIII</p>
-					<br><br>
 		<footer>
 					<p id="contact">
-					Contact: adminsour@sour.com 
+					Contact: adminsour@sour.com
 					<br>
 					Nr. telefon: +40 0751 123 234
 					</p>
-	</footer>	
-			
-	</main>			
-			
+	</footer>
 
-		
-	
-	
-			
+	</main>
+
+
+
+
+
+
 
 
 
